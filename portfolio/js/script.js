c50078f99926971b24103d05b1561c9d5f723e80
Nikita Shalamov <nikita.shalamovv@yandex.ru>
2024-02-22 12:44:08 +0500

    .

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -10,3 +10,45 @@ for (let smoothLink of smoothLinks) {
         });
     });
 };
+
+// const works__items = document.querySelectorAll('.works__item-photo img');
+const hoverLabels = document.querySelectorAll('.hover-labels');
+const items = document.querySelector('.works__items');
+
+const works__item_photos = document.querySelectorAll('.works__item-photo');
+
+function addBlur(target) {
+    target.querySelector('img').style.filter = 'blur(4px)';
+}
+
+function removeBlur(target) {
+    target.querySelector('img').style.filter = 'none';
+}
+
+function showLabels(target) {
+    target.querySelector('.hover-labels').classList.remove('hidden');
+}
+
+function hideLabels(target) {
+    target.querySelector('.hover-labels').classList.add('hidden');
+}
+
+works__item_photos.forEach(item => {
+    item.addEventListener('mouseenter', (e) => {
+        addBlur(e.currentTarget);
+        showLabels(e.currentTarget);
+    });
+
+    item.addEventListener('mouseleave', (e) => {
+        removeBlur(e.currentTarget);
+        hideLabels(e.currentTarget);
+    });
+
+    item.querySelector('.hover-labels').addEventListener('mouseenter', (e) => {
+        showLabels(e.currentTarget.parentElement);
+    });
+
+    item.querySelector('.hover-labels').addEventListener('mouseleave', (e) => {
+        hideLabels(e.currentTarget.parentElement);
+    });
+});
