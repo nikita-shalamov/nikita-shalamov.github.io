409f23404ee5957706302b709867953ed84c8233
Nikita Shalamov <nikita.shalamovv@yandex.ru>
2024-07-14 14:37:35 +0500

    created key-source

--- key-source/js/script.js
+++ key-source/js/script.js
@@ -1,53 +1,64 @@
 
 function openTab(tabId) {
-    // Скрываем все табы
     const tabs = document.querySelectorAll('.tab-content');
     tabs.forEach(tab => {
         tab.classList.remove('active');
     });
 
-    // Удаляем активный класс у всех кнопок
     const buttons = document.querySelectorAll('.tab-button');
     buttons.forEach(button => {
         button.classList.remove('active');
     });
 
-    // Показываем выбранный таб
     document.getElementById(tabId).classList.add('active');
 
-    // Добавляем активный класс к кнопке
     const activeButton = Array.from(buttons).find(button => button.getAttribute('onclick').includes(tabId));
     if (activeButton) {
         activeButton.classList.add('active');
     }
 }
 
-// Устанавливаем начальный таб
 document.addEventListener('DOMContentLoaded', () => {
     openTab('tab1');
 });
 
 
+function togglePromoInput() {
+    const promoInput = document.querySelector('.cart__promo-input');
+    if (promoInput.style.display === 'none' || promoInput.style.display === '') {
+        promoInput.style.display = 'block';
+    } else {
+        promoInput.style.display = 'none';
+    }
+}
+
+
+function toggleDropdown() {
+    const dropdown = document.getElementById('quantityDropdown');
+    dropdown.classList.toggle('open');
+}
+
+
+
 document.addEventListener('DOMContentLoaded', () => {
-    const modal = document.getElementById('modal');
     const modalOverlay = document.getElementById('modalOverlay');
     const openModalButton = document.getElementById('openModalButton');
     const closeButton = document.getElementById('closeButton');
 
     openModalButton.addEventListener('click', () => {
         modalOverlay.style.display = 'flex';
-        document.body.style.overflow = 'hidden'; // Disable scrolling
+        document.body.style.overflow = 'hidden';
     });
 
     closeButton.addEventListener('click', () => {
         modalOverlay.style.display = 'none';
-        document.body.style.overflow = 'auto'; // Enable scrolling
+        document.body.style.overflow = 'auto';
     });
 
     window.addEventListener('click', (event) => {
         if (event.target === modalOverlay) {
             modalOverlay.style.display = 'none';
-            document.body.style.overflow = 'auto'; // Enable scrolling
+            document.body.style.overflow = 'auto';
         }
     });
 });
