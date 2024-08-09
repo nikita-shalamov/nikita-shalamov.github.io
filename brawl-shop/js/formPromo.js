0f05e306b2650b5aeb41576dd600d5d15a893403
nikita-shalamov <nikita.shalamovv@yandex.ru>
2024-08-09 12:47:57 +0500

    change brawl-shop

--- brawl-shop/js/formPromo.js
+++ brawl-shop/js/formPromo.js
@@ -1,3 +1,18 @@
 document.getElementById('promo').addEventListener('input', function() {
     this.value = this.value.toUpperCase();
 });
+
+document.getElementById('balance').addEventListener('input', function (event) {
+    // Удаляем все нецифровые символы
+    let inputValue = event.target.value.replace(/[^\d]/g, '');
+    
+    // Форматируем число с пробелами
+    inputValue = inputValue.replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+
+    // Добавляем символ валюты
+    event.target.value = inputValue ? `${inputValue} ₽` : '';
+
+    // Устанавливаем позицию курсора перед символом валюты
+    const cursorPosition = inputValue.length;
+    event.target.setSelectionRange(cursorPosition, cursorPosition);
+});
